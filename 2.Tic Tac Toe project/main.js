c56ecb6f41e3ecd25006731e1ef7498d1a7a6ea0
shravan lingampally <kumashravan5@gmail.com>
2023-06-20 17:48:22 +0530

    completed tic tac toe

--- 2.Tic Tac Toe project/main.js
+++ 2.Tic Tac Toe project/main.js
@@ -1,43 +1,77 @@
-console.log("welcome to tic tac toe ");
-let audioturn = new Audio("sounds/ting.mp3");
+console.log("Welcome to Tic Tac Toe");
 let music = new Audio("sounds/music.mp3");
+let audioTurn = new Audio("sounds/ting.mp3");
 let gameover = new Audio("sounds/gameover.mp3");
-
-let changeturn = (turn) => {
-  if (turn === "X") {
-    return (turn = "0");
-  } else if (turn === "0") {
-    return (turn = "X");
-  }
-};
 let turn = "X";
-//function to check for a  win
-const checkwin = () => {
-  boxtexts = document.getElementsByClassName("boxtext");
+let isgameover = false;
+
+// Function to change the turn
+const changeTurn = () => {
+  return turn === "X" ? "0" : "X";
+};
+
+// Function to check for a win
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
   let wins = [
-    [0, 1, 2][3, 4, 5][6, 7, 8][0, 3, 6][1, 4, 7][2, 5, 8][0, 4, 8][ 2, 4, 6],
+    [0, 1, 2, 5, 5, 0],
+    [3, 4, 5, 5, 15, 0],
+    [6, 7, 8, 5, 25, 0],
+    [0, 3, 6, -5, 15, 90],
+    [1, 4, 7, 5, 15, 90],
+    [2, 5, 8, 15, 15, 90],
+    [0, 4, 8, 5, 15, 45],
+    [2, 4, 6, 5, 15, 135],
   ];
   wins.forEach((e) => {
     if (
-      (boxtexts[e[0]].innerText = boxtexts[e[1]]) && boxtexts[e[1]].innerText === boxtexts[e[2]]
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
     ) {
-      console.log(gandu);
+      document.querySelector(".info").innerText =
+        boxtext[e[0]].innerText + " Won";
+        music.play()
+      isgameover = true;
+      document
+        .querySelector(".imgagebox")
+        .getElementsByTagName("img")[0].style.width = "200px";
+      document.querySelector(
+        ".line"
+      ).style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+      document.querySelector(".line").style.width = "20vw";
     }
   });
 };
 
-// game main logic
+// Game Logic
+// music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
   element.addEventListener("click", () => {
-    if (element.innerText === "") {
-      element.innerText = turn;
-      turn = changeturn(turn);
-      audioturn.play();
-      checkwin();
-      document.getElementsByClassName(
-        "chanceinfo"
-      )[0].innerText = `Chance for ${turn}`;
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      turn = changeTurn();
+      audioTurn.play();
+      checkWin();
+      if (!isgameover) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
     }
   });
+});
+// Add onclick listener to reset button
+reset.addEventListener("click", () => {
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  turn = "X";
+  isgameover = false;
+  document.querySelector(".line").style.width = "0vw";
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+  document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width =
+    "0px";
 });
